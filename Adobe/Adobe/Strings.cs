3766185137cd51c02e52f3c4406a2123e563ba44
hemant bhatt <bhatthemant@live.com>
2020-08-22 19:06:44 +0530

    First commit for Strings.

---
+++ Adobe/Adobe/Strings.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Adobe
+{
+    public static class Strings
+    {
+        public static void LastIndexOfOne(string inputStr)
+        {
+            int lastIndex = -1;
+            for (int index = 0; index < inputStr.Length; index++)
+            {
+                if (inputStr[index] == '1')
+                {
+                    lastIndex = index;
+                }
+            }
+
+            Console.WriteLine(lastIndex);
+        }
+
+    }
+}
