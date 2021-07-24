a5f09f2f8d6f178f5468a1ce5dba4cf02b1566ec
hemant bhatt <bhatthemant@live.com>
2021-07-24 14:42:46 +0200

    Refactoring from other project.

---
+++ Algorithms/Searching/Searching/BinarySearch.cs
@@ -0,0 +1,46 @@
+﻿namespace Searching
+{
+    public static class BinarySearch
+    {
+        // Time complexity - O(log n)
+        // Space Complexity - O(log n)
+        public static int RecursiveSearch(int[] inputArr, int element, int low, int high)
+        {
+            if (low <= high)
+            {
+
+                int mid = low + (high - low) / 2;
+                if (inputArr[mid] == element)
+                    return mid;
+                if (inputArr[mid] > element)
+                    return RecursiveSearch(inputArr, element, low, mid - 1);
+                if (inputArr[mid] < element)
+                    return RecursiveSearch(inputArr, element, mid + 1, high);
+            }
+
+            return -1;
+        }
+
+        // Time complexity - O{log n)
+        // Space Complexity - O(1)
+        public static int IterativeSearch(int[] inputArr, int element)
+        {
+            int low = 0;
+            int high = inputArr.Length - 1;
+            while (low <= high)
+            {
+                int mid = low + (high - low) / 2;
+
+                if (inputArr[mid] == element)
+                    return mid;
+                if (inputArr[mid] > element)
+                    high = mid - 1;
+                else
+                    low = mid + 1;
+            }
+
+            return -1;
+        }
+
+    }
+}
---
+++ Algorithms/Searching/Searching/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Searching
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] inputArr = {10, 20, 80, 30, 60, 50,
+                     110, 100, 130, 170};
+            int[] inputSortedArr = {10, 20, 30, 50, 60, 80,
+                     100, 110, 130, 170};
+            int element = 110;
+
+            // Linear Search
+            Console.WriteLine($"Element found at {LinearSearch.Search(inputArr, element)}");
+            Console.WriteLine($"Element found at {LinearSearch.SearchImproved(inputArr, element)}");
+
+            // Binary Search
+            Console.WriteLine($"Element found at {BinarySearch.RecursiveSearch(inputSortedArr, element, 0, inputArr.Length - 1)}");
+            Console.WriteLine($"Element found at {BinarySearch.IterativeSearch(inputSortedArr, element)}");
+
+            element = 175;
+
+            // Linear Search
+            Console.WriteLine($"Element found at {LinearSearch.Search(inputArr, element)}");
+            Console.WriteLine($"Element found at {LinearSearch.SearchImproved(inputArr, element)}");
+
+            // Binary Search
+            Console.WriteLine($"Element found at {BinarySearch.RecursiveSearch(inputSortedArr, element, 0, inputArr.Length - 1)}");
+            Console.WriteLine($"Element found at {BinarySearch.IterativeSearch(inputSortedArr, element)}");
+        }
+    }
+}
