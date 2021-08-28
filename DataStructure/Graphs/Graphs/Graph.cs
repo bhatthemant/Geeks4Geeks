3438fcb685eb7d3919bcd1ae388f255ebd9e43b7
hemant bhatt <bhatthemant@live.com>
2021-08-28 19:47:29 +0200

    Added HackerEarth Problem

--- DataStructure/Graphs/Graphs/Graph.cs
+++ DataStructure/Graphs/Graphs/Graph.cs
@@ -1,12 +1,13 @@
+using System;
 using System.Collections.Generic;
 using System.Linq;
 
 namespace Graphs
-{ 
+{
     public class Vertex<T>
     {
         public Vertex(T value, params Vertex<T>[] parameters)
-            : this(value, (IEnumerable<Vertex<T>>) parameters)
+            : this(value, (IEnumerable<Vertex<T>>)parameters)
         {
         }
 
@@ -17,6 +18,8 @@ namespace Graphs
         }
 
         public T Value { get; set; } // can be made writable
+
+        public int Weight { get; set; } // Weight of the Edge
 
         public List<Vertex<T>> Neighbors { get; }
 
@@ -46,7 +49,7 @@ namespace Graphs
     public class GenericGraph<T>
     {
         public GenericGraph(params Vertex<T>[] initialNodes)
-            : this((IEnumerable<Vertex<T>>) initialNodes)
+            : this((IEnumerable<Vertex<T>>)initialNodes)
         {
         }
 
@@ -83,5 +86,80 @@ namespace Graphs
         {
             if (!first.Neighbors.Contains(second)) first.AddEdge(second);
         }
+
+        public void BreadthFirstTraversal(Vertex<T> startVertex)
+        {
+            if (!Vertices.Contains(startVertex))
+            {
+                Console.WriteLine("No such vertex found.");
+                return;
+            }
+
+            Queue<Vertex<T>> vertexQueue = new Queue<Vertex<T>>();
+            List<Vertex<T>> visitedVertices = new List<Vertex<T>>();
+
+            vertexQueue.Enqueue(startVertex);
+
+            while (vertexQueue.Count > 0)
+            {
+                var currentVertex = vertexQueue.Dequeue();
+
+                if (!visitedVertices.Contains(currentVertex))
+                {
+                    Console.WriteLine(currentVertex.Value);
+                    visitedVertices.Add(currentVertex);
+
+                    foreach (var neighbour in currentVertex.Neighbors)
+                    {
+                        vertexQueue.Enqueue(neighbour);
+                    }
+                }
+            }
+        }
+
+        // https://www.hackerearth.com/practice/algorithms/graphs/breadth-first-search/practice-problems/algorithm/bfs/
+        /*You have been given a Tree consisting of N nodes.
+         * A tree is a fully-connected graph consisting of N nodes and edges. 
+         * The nodes in this tree are indexed from 1 to N. 
+         * Consider node indexed 1 to be the root node of this tree.
+         * The root node lies at level one in the tree. 
+         * You shall be given the tree and a single integer x.
+         * You need to find out the number of nodes lying on level x. */
+        public void NodeAtLevel(Vertex<T> startVertex, int nodeLevel)
+        {
+            int currentLevel = 1;
+
+            if (!Vertices.Contains(startVertex))
+            {
+                Console.WriteLine("No such vertex found.");
+                return;
+            }
+
+            Queue<Vertex<T>> vertexQueue = new Queue<Vertex<T>>();
+            List<Vertex<T>> visitedVertices = new List<Vertex<T>>();
+            Dictionary<Vertex<T>, int> vertexLevel = new Dictionary<Vertex<T>, int>();
+
+            vertexQueue.Enqueue(startVertex);
+            vertexLevel.Add(startVertex, currentLevel);
+            while (vertexQueue.Count > 0)
+            {
+                var currentVertex = vertexQueue.Dequeue();
+                if (!visitedVertices.Contains(currentVertex))
+                {
+                    // Console.WriteLine(currentVertex.Value);
+                    visitedVertices.Add(currentVertex);
+                    foreach (var neighbour in currentVertex.Neighbors)
+                    {
+                        var currentVertexLevel = vertexLevel[currentVertex];
+                        vertexLevel[neighbour] = ++currentVertexLevel;
+                        vertexQueue.Enqueue(neighbour);
+                    }
+                }
+            }
+
+            Console.WriteLine($"Node at level {nodeLevel} is { vertexLevel.Values.Where(x => x.Equals(nodeLevel)).Count()}");
+        }
     }
+
+
 }
---
+++ DataStructure/Graphs/Graphs/Program.cs
@@ -0,0 +1,54 @@
+﻿namespace Graphs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GenericGraph<int> genericGraph = new GenericGraph<int>();
+
+            var vertex10 = new Vertex<int>(value: 10);
+            var vertex1 = new Vertex<int>(value: 1);
+            var vertex2 = new Vertex<int>(value: 2);
+            var vertex3 = new Vertex<int>(value: 3);
+            var vertex4 = new Vertex<int>(value: 4);
+            var vertex5 = new Vertex<int>(value: 5);
+            var vertex6 = new Vertex<int>(value: 6);
+            var vertex7 = new Vertex<int>(value: 7);
+            var vertex8 = new Vertex<int>(value: 8); 
+            var vertex9 = new Vertex<int>(value: 9); 
+            var vertex11 = new Vertex<int>(value: 11); 
+            var vertex12 = new Vertex<int>(value: 12);
+            var vertex13 = new Vertex<int>(value: 13);
+            var vertex14 = new Vertex<int>(value: 14);
+            var vertex15 = new Vertex<int>(value: 15);
+            var vertex16 = new Vertex<int>(value: 16);
+            var vertex17 = new Vertex<int>(value: 17);
+            var vertex18 = new Vertex<int>(value: 18);
+            var vertex19 = new Vertex<int>(value: 19);
+            var vertex20 = new Vertex<int>(value: 20);
+
+            genericGraph.AddPair(vertex11,vertex1);
+            genericGraph.AddPair(vertex1, vertex2);
+            genericGraph.AddPair(vertex13, vertex3);
+            genericGraph.AddPair(vertex15, vertex4);
+            genericGraph.AddPair(vertex17, vertex5);
+            genericGraph.AddPair(vertex11, vertex6);
+            genericGraph.AddPair(vertex2, vertex7);
+            genericGraph.AddPair(vertex1, vertex8);
+            genericGraph.AddPair(vertex15, vertex9); 
+            genericGraph.AddPair(vertex4, vertex10);
+            genericGraph.AddPair(vertex15, vertex12);
+            genericGraph.AddPair(vertex5, vertex13);
+            genericGraph.AddPair(vertex2, vertex14);
+            genericGraph.AddPair(vertex17, vertex15);
+            genericGraph.AddPair(vertex15, vertex16);
+            genericGraph.AddPair(vertex11, vertex17);
+            genericGraph.AddPair(vertex15, vertex18);
+            genericGraph.AddPair(vertex9, vertex19);
+            genericGraph.AddPair(vertex16, vertex20);
+
+            //genericGraph.BreadthFirstTraversal(vertex1);
+            genericGraph.NodeAtLevel(vertex1, 2);
+        }
+    }
+}
